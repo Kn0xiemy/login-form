dfabc8ad270923d09ac5103acfa0b00cd0b2bb35
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2023-08-09 10:56:04 +0300

    Partial push

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.main-container');
+  const loginContainer = document.querySelector('.login-container');
+  const registerContainer = document.querySelector('.register-container');
+
+  container.addEventListener('click', e => {
+    const registerFormBtn = e.target.closest('.registration');
+    const signUpBtn = e.target.closest('.signInBtn-link');
+
+    if (registerFormBtn) {
+      loginContainer.classList.add('slide');
+      registerContainer.classList.remove('slide');
+    }
+    if (signUpBtn) {
+      registerContainer.classList.add('slide');
+      loginContainer.classList.remove('slide');
+    }
+  });
+});
