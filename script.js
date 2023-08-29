a11cc4581cd0b46ce524209d54100868bf2f340d
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2023-08-29 15:48:55 +0300

    fixed visibility of the forms and button functions

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.main-container');
-  const loginContainer = document.querySelector('.login-container');
-  const registerContainer = document.querySelector('.register-container');
 
-  container.addEventListener('click', e => {
-    const registerFormBtn = e.target.closest('.registration');
-    const signUpBtn = e.target.closest('.signInBtn-link');
-
-    if (registerFormBtn) {
-      loginContainer.classList.add('slide');
-      registerContainer.classList.remove('slide');
-    }
-    if (signUpBtn) {
-      registerContainer.classList.add('slide');
-      loginContainer.classList.remove('slide');
-    }
-  });
-
- 
 });
 
+var loginForm = document.querySelector('#log-container');
+var registerForm = document.querySelector('#reg-container');
+
+
+function login() {
+  loginForm.classList.remove("hidden");
+  registerForm.classList.add("hidden");
+}
+
+function register() {  
+  loginForm.classList.add("hidden");
+  registerForm.classList.remove("hidden");
+}
+  // new way of being led to the accordion page, since the previous one was ugly and inefficient
+var navigateButton = document.querySelector(".login-btn")
+
+navigateButton.addEventListener("click", function() {
+  var accordionPage = "accordion.html";
+  window.location.href = accordionPage;
+})
 
